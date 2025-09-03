a1dac6c69220f5b646b9468450729104f6433c8c
BeatrizVezzali <beatrizandradevezzali@gmail.com>
2025-09-03 17:20:55 -0300

    Projeto Java

--- exemplojoptionpane/src/exemplojoptionpane/ExemploJOptionPane.java
+++ exemplojoptionpane/src/exemplojoptionpane/ExemploJOptionPane.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 
 public class ExemploJOptionPane {
 	public static void main(String[] args) {
-		String[] personagens = {"Bruxa do 71","Chapolin Colorado","Chaves","Chiquinha","Dona Florinda","Kiko","Professor Girafales",
+		String[] personagens = {"Bruxa do 71","Chapolin Colorado","Chiquinha","Chaves","Dona Florinda","Kiko","Professor Girafales",
 				"Seu Barriga","Seu Madruga"};
 		String resposta = (String)JOptionPane.showInputDialog(null,"Qual é o seu personagem favorito?",
 				"Pesquisa",JOptionPane.QUESTION_MESSAGE,
